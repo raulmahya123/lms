8234b66497c3b6bff99975dea1bb401246b4062c
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-09-07 03:17:58 +0700

    Fix Laravel -> quiz 1 (what is Laravel) -> Laravel intro quiz, 422, pada saat Kembali

--- resources/views/errors/422.blade.php
+++ resources/views/errors/422.blade.php
@@ -7,9 +7,10 @@
   <h1 class="text-7xl font-extrabold text-yellow-500">422</h1>
   <p class="mt-4 text-2xl font-semibold text-gray-800">Data Tidak Valid</p>
   <p class="mt-2 text-gray-600">Permintaan tidak bisa diproses karena ada data yang tidak valid.</p>
-  <a href="{{ url()->previous() }}" 
-     class="mt-6 inline-block px-6 py-3 bg-yellow-500 text-white rounded-lg shadow hover:bg-yellow-600">
-     Kembali
-  </a>
+<a href="javascript:void(0)"
+   onclick="if(history.length>1){history.back()}else{window.location='{{ route('home') }}'}"
+   class="mt-6 inline-block px-6 py-3 bg-yellow-500 text-white rounded-lg shadow hover:bg-yellow-600">
+  Kembali
+</a>
 </div>
 @endsection
