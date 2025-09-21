9b788d627468c01b9c6e7baac3265e6d20e623d9
git push <raulmahya11@gmail.com>
2025-09-21 20:57:55 +0700

    'push'

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  server: {
-    host: true,              // dengarkan semua interface
-    port: 5173,
-    cors: {
-      origin: ['https://berkemah.com'], // izinkan hanya domain produksi
-      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-      allowedHeaders: ['Content-Type','Authorization','X-Requested-With'],
-      credentials: false,
-    },
-    https: false, // biarkan false jika pakai ngrok yang sudah HTTPS
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
